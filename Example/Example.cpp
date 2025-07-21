538f0a6d43110e3997ff30b2271f45504626bed1
Matt Guerrette <direct3Dtutorials@gmail.com>
2025-07-21 00:15:54 -0400

    Add CMake build

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -1,88 +1,113 @@
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#else
+#include <cstdio>
+#endif
+
 #include "Example.h"
 
-
-ExampleStructure::ExampleStructure() : Structure(kStructureExample)
-{
-	// An instance of the ExampleStructure class is constructed for each
-	// occurrence of the "Example" structure in the OpenDDL file.
+ExampleStructure::ExampleStructure() : Structure(kStructureExample) {
+    // An instance of the ExampleStructure class is constructed for each
+    // occurrence of the "Example" structure in the OpenDDL file.
 }
 
-ExampleStructure::~ExampleStructure()
-{
+ExampleStructure::~ExampleStructure() {}
+
+bool ExampleStructure::ValidateSubstructure(
+    const DataDescription *dataDescription, const Structure *structure) const {
+    // In this example, we only allow the "float" data structure to be
+    // contained inside an "Example" structure.
+
+    return (structure->GetStructureType() == kDataFloat);
 }
 
-bool ExampleStructure::ValidateSubstructure(const DataDescription *dataDescription, const Structure *structure) const
-{
-	// In this example, we only allow the "float" data structure to be
-	// contained inside an "Example" structure.
+ExampleDataDescription::ExampleDataDescription() {}
 
-	return (structure->GetStructureType() == kDataFloat);
+ExampleDataDescription::~ExampleDataDescription() {}
+
+Structure *
+ExampleDataDescription::CreateStructure(const String<> &identifier) const {
+    // This function is called for each structure identifier encountered
+    // in an OpenDDL file. In this example, we only recognize the identifier
+    // "Example", and if matched, we return a new instance of ExampleStructure.
+
+    if (identifier == "Example") {
+        return (new ExampleStructure);
+    }
+
+    return (nullptr);
 }
 
-
-ExampleDataDescription::ExampleDataDescription()
+#ifdef _WIN32
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance,
+                   LPSTR commandLine, int cmdShow)
+#else
+int main(int argc, char *argv[])
+#endif
 {
-}
+    char *buffer;
+    // Parse the file "Example.oddl".
+#ifdef _WIN32
+    HANDLE handle =
+        CreateFile("Example.oddl", GENERIC_READ, FILE_SHARE_READ, nullptr,
+                   OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+    if (handle != INVALID_HANDLE_VALUE) {
+        DWORD actual;
 
-ExampleDataDescription::~ExampleDataDescription()
-{
-}
+        // Allocate space for the entire file plus one more byte for the zero
+        // terminator.
 
-Structure *ExampleDataDescription::CreateStructure(const String<>& identifier) const
-{
-	// This function is called for each structure identifier encountered
-	// in an OpenDDL file. In this example, we only recognize the identifier
-	// "Example", and if matched, we return a new instance of ExampleStructure.
+        DWORD size = GetFileSize(handle, nullptr);
+        buffer = new char[size + 1];
 
-	if (identifier == "Example")
-	{
-		return (new ExampleStructure);
-	}
+        // Read the entire contents of the file and put a zero terminator at the
+        // end.
 
-	return (nullptr);
-}
+        ReadFile(handle, buffer, size, &actual, nullptr);
+        buffer[size] = 0;
+        CloseHandle(handle);
+    }
+#else
+    // Read the file "Example.oddl" into memory.
+    FILE *file = fopen("Example.oddl", "rb");
+    if (file) {
+        // Allocate space for the entire file plus one more byte for the zero
+        // terminator.
+        fseek(file, 0, SEEK_END);
+        long size = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        buffer = new char[size + 1];
+        // Read the entire contents of the file and put a zero terminator at the
+        // end.
+        size_t actual = fread(buffer, 1, size, file);
+        fclose(file);
+        buffer[size] = 0;
+    }
+#endif
 
+    ExampleDataDescription exampleDataDescription;
 
-int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLine, int cmdShow)
-{
-	// Parse the file "Example.oddl".
+    // Once the file is in memory, the DataDescription::ProcessText() function
+    // is called to create the structure tree.
 
-	HANDLE handle = CreateFile("Example.oddl", GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
-	if (handle != INVALID_HANDLE_VALUE)
-	{
-		ExampleDataDescription	exampleDataDescription;
-		DWORD					actual;
+    DataResult result = exampleDataDescription.ProcessText(buffer);
+    if (result == kDataOkay) {
+        const Structure *structure =
+        exampleDataDescription.GetRootStructure()->GetFirstSubstructure(kStructureExample);
+        while (structure) {
+            // This loops over all top-level structures in the file.
 
-		// Allocate space for the entire file plus one more byte for the zero terminator.
+            // Do something with the data...
+            printf("Found structure: %s\n",
+                     structure->GetStructureName());
 
-		DWORD size = GetFileSize(handle, nullptr);
-		char *buffer = new char[size + 1];
+            structure = structure->GetNextSubnode();
+        }
+    }
 
-		// Read the entire contents of the file and put a zero terminator at the end.
+    delete[] buffer;
 
-		ReadFile(handle, buffer, size, &actual, nullptr);
-		buffer[size] = 0;
-
-		// Once the file is in memory, the DataDescription::ProcessText() function
-		// is called to create the structure tree.
-
-		DataResult result = exampleDataDescription.ProcessText(buffer);
-		if (result == kDataOkay)
-		{
-			const Structure *structure = exampleDataDescription.GetRootStructure()->GetFirstSubnode();
-			while (structure)
-			{
-				// This loops over all top-level structures in the file.
-
-				// Do something with the data...
-
-				structure = structure->GetNextSubnode();
-			}
-		}
-
-		delete[] buffer;
-	}
-
-	return (0);
+    return (0);
 }
